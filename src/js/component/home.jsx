716694a881d262099565f0383aac5b33db66d4e5
Alejandro <64385399+Fractal-Vortex@users.noreply.github.com>
2024-11-06 13:02:26 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,55 @@
+import { useState } from "react";
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [textoDeEntrada, setTextoDeEntrada] = useState("");
+	const [tareas, setTareas] = useState([]);
+
+	const controlTarea = (e) => {
+		setTextoDeEntrada(e.target.value)
+	}
+
+	const controlInputTexto = (e) => {
+		e.preventDefault(); 
+		if (textoDeEntrada.trim() === "")  return; 
+
+		const nuevaTarea = { label: textoDeEntrada, id: Date.now() };
+		setTareas([...tareas, nuevaTarea]);
+		setTextoDeEntrada("");
+
+	};
+
+	const eliminarTarea = (indice) => {
+		const eliminar = tareas.filter((_, i) => i !== indice);
+		setTareas(eliminar);
+	}
+
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>To Do List</h1>
+			<form onSubmit={controlInputTexto} className="formulario">
+				<input
+					onChange={controlTarea}
+					value={textoDeEntrada}
+					type="text"
+					placeholder="Agrega tareas" />
+				<input className="addBtn" type="submit" value="Add" />
+			</form>
+			<ul>
+				{
+					tareas.map((tarea, indice) => (
+						<li key={tarea.id}>
+							{tarea.label}
+							<span
+								onClick={() => eliminarTarea(indice)}
+								className="papelera fa-regular fa-trash-can"
+							></span>
+						</li>
+					))}
+			</ul>
+			<div className="contador">{`${tareas.length} Tareas`}</div>
 		</div>
 	);
 };
